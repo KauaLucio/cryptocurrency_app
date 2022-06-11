d7bc12691ed2a8ae1dde32bf40a2513202113a72
Kauã Lúcio <kauadefreitas.s992@gmail.com>
2022-06-11 19:57:22 -0300

    last commit

--- src/components/CoinConversion.tsx
+++ src/components/CoinConversion.tsx
@@ -5,10 +5,10 @@ const CoinConversion = ({quotation, priceCoinUSD}: any) => {
   return (
     <div className="rounded-lg shadow-md bg-gradient-to-r from-[#4776e6] to-[#8e54e9] p-3 text-white">
       <h3 className="font-bold text-lg mb-2">{quotation.name}</h3>
-      <p className="text-2xl">{quotation.code} <span className="font-medium ">${millify(Number(priceCoinUSD), {
+      <p className="text-2xl">{quotation.code} <span className="font-medium ">{millify(Number(priceCoinUSD), {
         precision: 3
       })}</span></p>
-      <p className="text-2xl">{quotation.codein} <span className="font-medium ">R${millify(coinInBRL, {
+      <p className="text-2xl">{quotation.codein} <span className="font-medium ">{millify(coinInBRL, {
         precision: 3
       })}</span></p>
     </div>
